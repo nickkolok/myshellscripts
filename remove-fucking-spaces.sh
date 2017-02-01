79ace74778131b9dd739d441ff453e3b34a5afc4
nickkolok <nickkolok@mail.ru>
2017-02-01 15:51:48 +0300

    Экранирование ведущего минуса в именах файлов

--- remove-fucking-spaces.sh
+++ remove-fucking-spaces.sh
@@ -2,3 +2,5 @@
 rename --verbose 's/\s\(1\)/___1/g' -- *\ \(1\).*
 rename --verbose 's/[\s\!\(\)]/_/g' -- *
 
+# Escape leading minus because it is treated as beginning of option list
+rename --verbose 's/^-/_-/g' -- *
